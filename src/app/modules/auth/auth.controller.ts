ea2e753914814ce7a0ebf8428ee163b28b37e943
senseix21 <mahdix2118@protonmail.com>
2023-07-02 09:39:42 -0400

    created auth controller for user login

---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import config from "../../../config";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import { ILoginUserResponse } from "./auth.interface";
+
+
+const loginUser = catchAsync(async (req: Request, res: Response) => {
+    const { ...loginData } = req.body;
+    const result = await AuthService.loginUser(loginData);
+    const { refreshToken, ...others } = result;
+
+    // set refresh token into cookie
+
+    const cookieOptions = {
+        secure: config.env === 'production',
+        httpOnly: true,
+    };
+
+    res.cookie('refreshToken', refreshToken, cookieOptions);
+
+    sendResponse<ILoginUserResponse>(res, {
+        statusCode: 200,
+        success: true,
+        message: 'User loggedin successfully !',
+        data: others,
+    });
+});
+
+export const AuthController = {
+    loginUser
+}
