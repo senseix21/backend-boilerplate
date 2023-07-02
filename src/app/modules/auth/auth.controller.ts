39a938e12015a875d3e2a73af9fca07469fb0108
senseix21 <mahdix2118@protonmail.com>
2023-07-02 13:01:28 -0400

    created refresh token controller

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import config from "../../../config";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
-import { ILoginUserResponse } from "./auth.interface";
+import { ILoginUserResponse, IRefreshTokenResponse } from "./auth.interface";
 import { AuthService } from "./auth.service";
 
 
@@ -28,6 +28,30 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
     });
 });
 
+
+const refreshToken = catchAsync(async (req: Request, res: Response) => {
+    const { refreshToken } = req.cookies;
+
+    const result = await AuthService.refreshToken(refreshToken);
+
+    // set refresh token into cookie
+
+    const cookieOptions = {
+        secure: config.env === 'production',
+        httpOnly: true,
+    };
+
+    res.cookie('refreshToken', refreshToken, cookieOptions);
+
+    sendResponse<IRefreshTokenResponse>(res, {
+        statusCode: 200,
+        success: true,
+        message: 'User loggedin successfully !',
+        data: result,
+    });
+});
+
 export const AuthController = {
-    loginUser
+    loginUser,
+    refreshToken
 }
